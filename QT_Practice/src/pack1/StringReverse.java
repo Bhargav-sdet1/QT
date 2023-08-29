d66cd0e5d9fa99a957ac6e70cf4c885f163da8a8
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-08-29 23:39:49 +0530

    Programs

---
+++ QT_Practice/src/pack1/StringReverse.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+
+		String name="Bhargav Goud";
+		
+		for(int i=name.length()-1;i>=0;i--)
+		{
+			System.out.print(name.charAt(i));
+		}
+
+	}
+
+}
