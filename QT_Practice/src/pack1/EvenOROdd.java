4b52065c89e3cf67e0fa0de9231d423c2cbfa00f
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-09-01 17:29:53 +0530

    EvenOdd

---
+++ QT_Practice/src/pack1/EvenOROdd.java
@@ -0,0 +1,17 @@
+package pack1;
+
+public class EvenOROdd {
+
+	public static void main(String[] args) {
+		
+		int num =10;
+		
+		if(num%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+			System.out.println("Odd");
+	}
+
+}
