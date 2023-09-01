4ca38c4a0b197e0364c217937ff79c8ab7f52c61
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-09-01 15:03:06 +0530

    Program

--- QT_Practice/src/pack1/StringReverse.java
+++ QT_Practice/src/pack1/StringReverse.java
@@ -5,12 +5,11 @@ public class StringReverse {
 	public static void main(String[] args) {
 
 		String name="Bhargav Goud";
-		
-		for(int i=name.length()-1;i>=0;i--)
-		{
-			System.out.print(name.charAt(i));
-		}
-
+	
+	for(int i=name.length()-1;i>=0;i--)
+	{
+		System.out.print(name.charAt(i));
 	}
+}
 
 }
