83496d361d200ed8a69ceac944e8d2ac4c39ba76
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-09-13 21:39:33 +0530

    prog

---
+++ Java_Programs/src/programs/StringBufferEx.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class StringBufferEx {
+
+	public static void main(String[] args) {
+		
+		String str = "Bhargav Goud Pandula";
+		
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(sb.reverse());
+	}
+
+}
---
+++ Java_Programs/src/programs/TerinaryOperator.java
@@ -0,0 +1,14 @@
+package programs;
+
+public class TerinaryOperator {
+
+	public static void main(String[] args) {
+
+		int a =10, b=20;
+		
+		int x= a>b ? a:b;
+		System.out.println(x);
+
+	}
+
+}
