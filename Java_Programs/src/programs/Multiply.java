5c98cab6d446c0201b7d4dec21a3669ffa607ab5
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-09-16 22:46:55 +0530

    x,y

---
+++ Java_Programs/src/programs/Multiply.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int x=1, y=2, z=3;
+		
+		System.out.println(x+y*z);
+		System.out.println((x+y)*z);
+
+	}
+
+}
