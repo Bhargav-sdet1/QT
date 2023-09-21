8242172c75d2aa3356a04430181482fe31e6ceb3
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-09-21 06:59:56 +0530

    20/9/23

---
+++ AutomationPracticeQT/src/javaPack/DropdownSelection.java
@@ -0,0 +1,48 @@
+package javaPack;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DropdownSelection {
+	
+	WebDriver driver;
+	
+	@Test
+	public void cssSelector() throws InterruptedException
+	{
+		driver.get("https://retail.onlinesbi.sbi/");
+		
+		Actions act = new Actions(driver);
+		Thread.sleep(5000);
+		act.moveToElement(driver.findElement(By.xpath("//ul[@id='primary_navigation']/li[10]/a"))).perform();
+		Thread.sleep(5000);
+		act.moveToElement(driver.findElement(By.xpath("//ul[@id='primary_navigation']/li[10]/a/following-sibling::ul/li[2]"))).perform();
+		Thread.sleep(5000);
+		driver.findElement(By.linkText("MCA Current Account")).click();
+		
+		Thread.sleep(2000);
+		
+	}
+	@BeforeTest
+	public void setUp()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+	}
+	
+
+}
--- AutomationPracticeQT/src/javaPack/KeyboardActions.java
+++ AutomationPracticeQT/src/javaPack/KeyboardActions.java
@@ -1,35 +1,37 @@
 package javaPack;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class MouseActions {
-	
-	WebDriver driver;
+public class KeyboardActions {
+WebDriver driver;
 	
 	@Test
 	public void cssSelector() throws InterruptedException
 	{
-		driver.get("https://retail.onlinesbi.sbi/");
+		driver.get("https://www.tsrtconline.in/oprs-web/guest/home.do?h=1");
 		
-		Actions act = new Actions(driver);
-		Thread.sleep(5000);
-		act.moveToElement(driver.findElement(By.xpath("//ul[@id='primary_navigation']/li[10]/a"))).perform();
-		Thread.sleep(5000);
-		act.moveToElement(driver.findElement(By.xpath("//ul[@id='primary_navigation']/li[10]/a/following-sibling::ul/li[2]"))).perform();
-		Thread.sleep(5000);
-		driver.findElement(By.linkText("MCA Current Account")).click();
+		driver.findElement(By.xpath("//input[@name='fromPlaceName']")).sendKeys("Hyd");
 		
+		List<WebElement> dd=driver.findElements(By.xpath("//ul[@id='ui-id-1']//li"));
 		Thread.sleep(2000);
-		
+		System.out.println(dd.size());
+		for(WebElement list:dd)
+		{
+			if(list.getText().contains("HYDERABAD DARSHAN 3"))
+			{
+				list.click();
+				break;
+			}
+		}
 	}
 	@BeforeTest
 	public void setUp()
@@ -38,11 +40,10 @@ public class MouseActions {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
-	@AfterTest
+	//@AfterTest
 	public void tearDown()
 	{
 		driver.close();
 	}
-	
 
 }
