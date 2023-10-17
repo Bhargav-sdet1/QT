28ebe4db2b6d7505719ba80576dc751a8399950b
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-10-17 22:54:12 +0530

    swap

---
+++ Java_Programs/src/nalabs_Programs/Swap2NumbersWithord3rd.java
@@ -0,0 +1,18 @@
+package nalabs_Programs;
+
+public class Swap2NumbersWithord3rd {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		
+		System.out.println("x is:"+x);
+		System.out.println("y is:"+y);
+
+	}
+
+}
