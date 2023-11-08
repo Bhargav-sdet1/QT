baaa15cc24dc4b5b9bf07fce66514ad655fa84ff
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-11-08 18:24:42 +0530

    8/11

---
+++ Sample/src/test/LargestNumber.java
@@ -0,0 +1,18 @@
+package test;
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+
+		int a=100;
+		int b=20;
+		
+		if(a>b) {
+			System.out.println("a is largest");
+		}
+		else
+			System.out.println("b is largest");
+
+	}
+
+}
