55ed5b5b13e68251b1c541f0fc7e0ff00ba9e21a
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-11-20 23:25:06 +0530

    20/11

---
+++ Java_Programs/src/nalabs_Programs/DivideByZero.java
@@ -0,0 +1,14 @@
+package nalabs_Programs;
+
+public class DivideByZero {
+
+	public static void main(String[] args) {
+
+		System.out.println(10.0/0);
+		System.out.println(10/0.0);
+		System.out.println(123.23f/0);
+		System.out.println(0.0/0.0);
+
+	}
+
+}
